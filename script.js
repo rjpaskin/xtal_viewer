09b64c8c1469e206ae7f16181630de79d673c889
Rob Paskin <rjpaskin+git@gmail.com>
2012-07-02 13:26:55 +0100

    Details on hover, fixes.

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ $(function() {
     
     console.log(root.find('condition').slice(0,2));
     
-    var table = $('<table/>');
+    var table = $('#ingredients tbody');
     
     // IDs <-> ingredient map:
-    //  key:   stock ID
-    //  value: eq() of ingredient in list
+    //     key:   stock ID
+    //     value: eq() of ingredient in list
     var ingredients = {};
     
+    console.log(root.find('stock').length);
+    
+    // Setup table of ingredients
     root.find('ingredient').each(function(ind) {
       var el = $(this);
       var ids = el.find('localID').map(function() {
@@ -27,8 +30,8 @@ $(function() {
         //.append('<td>' + ids.join(', ') + '</td>')
         .appendTo(table);        
     });
-    table.appendTo('body').attr('id', 'ingredients');        
     
+    // Setup plate view
     var list = $('#conditions');
     
     root.find('condition').each(function(ind) {
@@ -44,23 +47,55 @@ $(function() {
       // Match IDs to actual ingredients
       $.each(ids, function(ind, id) {
         if (ingredients.hasOwnProperty(id)) {
-          my_ingreds.push(ingred_els.eq(ingredients[id]).find('shortName').text());
+          my_ingreds.push(ingred_els.eq(ingredients[id]).clone());
         }
       });
       
-      var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-      var my_lett = letters[Math.ceil((ind + 1) / 12) - 1];
+      var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+          my_lett = letters[Math.ceil((ind + 1) / 12) - 1]
+          cell_id = my_lett + ((ind % 12) + 1);
       
       $('<li/>')
+        .data({
+          'ingredients': my_ingreds,
+          'conditions':  el.find('conditionIngredient').clone()
+        })
         .append(function() {
           var sub = $('<ul/>').addClass('list');
           $.each(my_ingreds, function(ind, val) {
-            sub.append('<li>' + val + '</li>');
+            sub.append('<li>' + val.find('shortName').text() + '</li>');
           });
           return sub;
         })
-        .append('<span class="cell-id">' + my_lett + ((ind % 12) + 1) + '</span>')
+        .append('<span class="num-conditions">' + my_ingreds.length + '</span>')
+        .append('<span class="cell-id">' + cell_id + '</span>')
         .appendTo(list);        
     });
+  });
+  
+  $('#conditions')
+  .on('mouseover', 'li:not(.list li)', function() {
+    var data = $(this).data();
+    
+    var details = $('#details ul').empty();
+    
+    $.each(data.ingredients, function(ind, val) {
+      var cond = $(data.conditions[ind]);
+      var id   = cond.find('stockLocalID').text();
+            
+      // Find appropriate stock
+      var stock = window._root.find('stock').eq(parseInt(id) - 1);
+      
+      $('<li/>')
+        .append(cond.find('concentration').text())
+        .append(stock.find('units').text())
+        .append(' ')
+        .append(val.find('name').text())
+        .append(' (' + cond.find('type').text().toLowerCase() + ')')
+        .appendTo(details);
+    });
+  })
+  .on('mouseout', function() {
+    $('#details ul').empty();
   });
 });
