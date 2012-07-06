1e9748b42bc2a10db9ca1a8c7a6152ef2b78c384
Rob Paskin <rjpaskin+git@gmail.com>
2012-07-06 14:43:16 +0100

    PHP-passthru to ruby now RVM-aware

--- list_screen_files.php
+++ list_screen_files.php
@@ -1,3 +1,10 @@
 <?php
 header('Content-Type: application/json');
-system('ruby list_screen_files.rb');
+
+$ruby = 'ruby';
+
+if (file_exists($_ENV['HOME'] . '/.rvm/bin/rvm')) {
+  $ruby = "~/.rvm/rubies/ruby-1.9.2-p290/bin/ruby";
+}
+
+system("$ruby list_screen_files.rb");
