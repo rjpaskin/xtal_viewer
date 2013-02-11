104e8808ca1d020218ced8999a8861188dad412e
Rob Paskin <rjpaskin+git@gmail.com>
2013-02-11 16:54:08 +0000

    Fix screen listings under rbenv

--- list_screen_files.php
+++ list_screen_files.php
@@ -6,5 +6,8 @@ $ruby = 'ruby';
 if (file_exists($_ENV['HOME'] . '/.rvm/bin/rvm')) {
   $ruby = "~/.rvm/rubies/ruby-1.9.2-p290/bin/ruby";
 }
+elseif (file_exists($_ENV['HOME'] . '/.rbenv')) {
+  $ruby = 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; ruby';
+}
 
 system("$ruby list_screen_files.rb");
